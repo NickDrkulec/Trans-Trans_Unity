2a20a36cd954e361aa0c5334f88a3409d659b099
Ryan Talbot <dtalbot@stedwards.edu>
2021-10-27 11:22:51 -0500

    Edited scripts for legibility (no changes to the actual code)

--- Transpiration Transformation Steds/Assets/Scripts/Manager.cs
+++ Transpiration Transformation Steds/Assets/Scripts/Manager.cs
@@ -5,31 +5,24 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-
     public GameObject[] Levels;
     public GameObject ResetScreen, End;
 
     int currentLevel;
 
-    //when question answered wrong nothing happens for now but this will be the code for something to happen
+    // When question answered wrong nothing happens for now but this will be the code for something to happen
     public void wrongAnswer()
     {
         ResetScreen.SetActive(true);
     }
 
 
-
-
-
-
-
-    //when a question is answered correctly, the next level is loaded. If there is no next level to load, it will load an "end scene"
+    // When a question is answered correctly, the next level is loaded. If there is no next level to load, it will load an "end scene"
     public void correctAnswer()
     {
         if (currentLevel + 1 != Levels.Length)
         {
             Levels[currentLevel].SetActive(false);
-
             currentLevel++;
             Levels[currentLevel].SetActive(true);
         }
